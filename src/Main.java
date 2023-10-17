85797b808bf1e3b5295ead623f5b3c6ed784192c
Allana Larissa <larissaleonel98@gmail.com>
2023-10-17 16:15:28 -0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import entinties.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+        System.out.print("How many employees will be registered? ");
+        int N = sc.nextInt();
+
+        for (int i=0; i<N; i++) {
+            System.out.println("Emplyoee #" + (i + 1) + ":");
+            System.out.print("Id: ");
+            Integer id = sc.nextInt();
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Salary: ");
+            Double salary = sc.nextDouble();
+
+            Employee emp = new Employee(id, name, salary);
+
+            list.add(emp);
+        }
+        System.out.println();
+        System.out.print("Enter the employee id that will have salary increase : ");
+        int idsalary = sc.nextInt();
+
+    }
+    public static Integer position(List<Employee> list, int id) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id) {
+                return i;
+            }
+        }
+        return null;
+    }
+    public static boolean hasId(List<Employee> list, int id) {
+        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        return emp != null;
     }
 }
